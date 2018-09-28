5941913a9455c2096954aad4102ee16a5d1f3599
a89881961 <a89881961@gmail.com>
2018-09-28 15:34:57 +0800

    update

---
+++ src/js/component/hrtest.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+const lists = ['a','b','c','d'];
+export default class Hrtest extends Component{
+    render() {
+        return(
+            <ul>
+                123
+                {
+                    lists.map((result) => {
+                        return (
+                            <li className="text-center">{result}</li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
---
+++ src/js/component/test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+export default class Test extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '123',
+            count: '456',
+            buttonName: '編輯'
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        return alert(123)
+    }
+    render() {
+        const {name} = this.state
+        //const {somethng} = this.proop
+            return (
+                
+                <div>
+                    <button onClick={this.handleClick}>
+                        編輯
+                    </button>
+                    <p>Hello, {name}{this.state.count}!</p>
+                </div>  
+            )
+        
+        
+    }
+}
+
+
+
