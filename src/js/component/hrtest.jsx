7ca694d0ffe7a6feb8c5fc9b373386a1a24d78a5
lagane100 <lagane100@gmail.com>
2018-10-04 21:33:05 +0800

    change code

--- src/js/component/hrtest.jsx
+++ src/js/component/hrtest.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class Hrtest extends Component{
+export default class Hrtest extends React.Component{
     render() {
         return(
             <h1>123</h1>
